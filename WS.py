d6f17a1d066e7c132ac5bce101d6d98886e621d4
axelsolea <axel.chalvin@etu.univ-smb.fr>
2025-12-12 14:52:52 +0100

    Feat : (SOAP) ForwardGeoCoding

--- WS.py
+++ WS.py
@@ -1,11 +1,13 @@
+import json
 from wsgiref.simple_server import make_server
-from spyne import Application, rpc, ServiceBase, Integer, Iterable, Unicode, Float
+from spyne import Application, rpc, ServiceBase, Integer, Iterable, Unicode, Float, String
 from spyne.protocol.soap import Soap11
 from spyne.server.wsgi import WsgiApplication
 import math
 import requests
 
 OpenRteSce_API_KEY = "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjBhYTQ4MWRiYzJjMzQ5NWRiYzhhZTEyOGM4ZDZhNGM5IiwiaCI6Im11cm11cjY0In0="
+OpenCage_API_KEY = "91f9a68a0eb04dd492e16b64e74faca0"
 
 class ComputeTravelTime(ServiceBase):
     @rpc(float, float, float, _returns=float)      #Déclaration de fonction SOAP
@@ -16,7 +18,7 @@ class ComputeTravelTime(ServiceBase):
         return temps_trajet
 
 class NearChargingStations(ServiceBase):
-    @rpc(Float, Float, Integer, _returns=Iterable(Unicode))
+    @rpc(Float, Float, Integer, _returns=Iterable(str))
     def near_charging(ctx, long, lat, radius):
         url = "https://odre.opendatasoft.com/api/explore/v2.1/catalog/datasets/bornes-irve/records?limit=5&where=within_distance(geo_point_borne,geom'POINT({long} {lat})', {radius})"
         long = float(long)
@@ -26,11 +28,24 @@ class NearChargingStations(ServiceBase):
         payload = {};headers = {}
         print("[DEBUG] Req  url = %s" % requestUrl)
         response = requests.request("GET", requestUrl, headers=headers, data=payload)
-        print(response.json())
-        return response.json()
+        return response.text
+
+class forwardGeocoding(ServiceBase):
+    @rpc(String, _returns=Iterable(Unicode))
+    def forward(ctx, name):
+        url="https://api.opencagedata.com/geocode/v1/json?key={key}&q={name}&limit=3&pretty=1"
+        payload={};headers = {}
+        requestUrl = url.format(key=OpenCage_API_KEY, name=name)
+
+        print("[DEBUG] Req  url = %s" % requestUrl)
+        response = requests.request("GET", requestUrl, headers=headers, data=payload)
+        response = response.json()
+        formattedResponse = [str(response["results"][0]["formatted"]),str(response["results"][0]["geometry"]["lat"]), str(response["results"][0]["geometry"]["lng"])]
+        return formattedResponse
+
 
 application = Application(
-    [ComputeTravelTime, NearChargingStations],            # Liste des services exposés
+    [ComputeTravelTime, NearChargingStations, forwardGeocoding],            # Liste des services exposés
     'localhost/travel',                 # Namespace du service
     in_protocol=Soap11(validator='lxml'),   # Protocole SOAP en entrée
     out_protocol=Soap11())                   # Protocole SOAP en sortie
--- WServer.py
+++ WServer.py
@@ -1,16 +1,13 @@
 from flask import Flask, render_template, request
 import folium
 import zeep
+import json
 app = Flask(__name__)
 
 #APIZeep définition du Sce Web
 wsdl = 'http://127.0.0.1:8000/?wsdl'
 client = zeep.Client(wsdl=wsdl)
 
-@app.route("/") #Render de l'index
-def index():
-    return render_template('travelTimeForm.html')
-
 @app.route("/compute", methods=['GET','POST']) #Render de l'index avec calcul
 def compute():
     try:
@@ -25,19 +22,68 @@ def compute():
     except Exception as e: #Affichage de l'erreur en cas d'erreur
         return render_template("travelTimeForm.html", erreur=str(e))
 
-@app.route("/compute", methods=['GET','POST']) #Render de l'index avec calcul
-def compute():
+@app.route("/")
+def components():
+    """Extract map components and put those on a page."""
+    m = folium.Map(
+        width=800,
+        height=600,
+    )
+
+    m.get_root().render()
+    header = m.get_root().header.render()
+    body_html = m.get_root().html.render()
+    script = m.get_root().script.render()
+
+    return render_template("index.html", header=header, body_html=body_html, script=script)
+
+@app.route("/computeTravel", methods=['GET','POST'])
+def componentsCompute():
     try:
-        distance = request.form['distance']
-        evRange = request.form['evRange']
-        chargeTime = request.form['chargeTime']
-        print("[DEBUG] Variables récupérées : distance=" + str(distance) + ", evRange=" + str(evRange) + ", chargeTime=" + str(chargeTime))
-        result = client.service.compute(distance, evRange, chargeTime) #Requêtage du Sce Web avec APIZeep
-        print("[DEBUG] Résultat: " + str(result))
-        return render_template("travelTimeForm.html", resultat=result)
+        """Extract map components and put those on a page."""
+        m = folium.Map(
+            width=800,
+            height=600,
+        )
+        #Form values
+        start=request.form['start']
+        end=request.form['end']
+
+        #Forward result format : ["formatted address","latitude","longitude"]
+        forwardStartResult = client.service.forward(start)
+        forwardEndResult = client.service.forward(end)
+
+        #Marqueur origine
+        folium.Marker(
+            location=[forwardStartResult[1],forwardStartResult[2]],
+            tooltip="Click me!",
+            popup=forwardStartResult[0], #ADRESSE SOURCE
+            icon=folium.Icon(color="green"),
+        ).add_to(m)
+
+        #Marqueur destination
+        folium.Marker(
+            location=[forwardEndResult[1],forwardEndResult[2]],
+            tooltip="Click me!",
+            popup=forwardEndResult[0],  #ADRESSE DEST
+            icon=folium.Icon(color="red"),
+        ).add_to(m)
+
+        #Zoom auto & limites de carte
+        south_west = [min(forwardStartResult[1], forwardEndResult[1]), min(forwardStartResult[2], forwardEndResult[2])]
+        north_east = [max(forwardStartResult[1], forwardEndResult[1]), max(forwardStartResult[2], forwardEndResult[2])]
+        m.fit_bounds([south_west, north_east])
+
+        #Rendu de la carte
+        m.get_root().render()
+        header = m.get_root().header.render()
+        body_html = m.get_root().html.render()
+        script = m.get_root().script.render()
+
+        return render_template("index.html", header=header, body_html=body_html, script=script)
 
     except Exception as e: #Affichage de l'erreur en cas d'erreur
-        return render_template("travelTimeForm.html", erreur=str(e))
+        return render_template("index.html", erreur=str(e))
 
 
 if __name__ == "__main__":
